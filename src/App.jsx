bc10d6cf18929835e418b5ce8e53e1c35103d08c
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-09-24 17:38:48 +0530

    Add Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -18,6 +19,8 @@ function App() {
       <Learning />
       <Contact />
       <Footer />
+
+      <Analytics />
     </>
   );
 }
